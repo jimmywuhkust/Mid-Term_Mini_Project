e0a8bdc9b0b33e50667870a2c258f4270feb1837
Jimmy WU <cmwuaa@connect.ust.hk>
2024-10-24 21:13:40 +0800

    Added Product Review

--- ISDN_3150_Mid-Term/src/Products.jsx
+++ ISDN_3150_Mid-Term/src/Products.jsx
@@ -123,7 +123,7 @@ export const products = [
     Tagline: 'Erase others’ answers, Ace your exams!',
     description: 'Ace your exams with style! ClearGaze X is the ultimate smart glasses designed to help you stay ahead of the competition. Equipped with an invisible laser, these glasses can erase answers on others’ exam papers—so you can boost your marks without anyone knowing!',
     stock: 10,
-    review: 'Review of Product A',
+    review: '⭐⭐',
     thumbnail: 'product/Glasses.png',
     threeDModel: 'product/shadow_glasses_rot.glb'
   },
@@ -135,7 +135,7 @@ export const products = [
     Tagline: "Be Present, Even When You're Not!",
     description: 'The Holo Wand is your ultimate academic companion, empowering you to attend classes virtually while managing your real-world commitments. With its advanced holographic technology, you can project a lifelike avatar, engage in discussions, and earn those attendance points—all from the comfort of your own space. Embrace the future of learning with Holo Wand!',
     stock: 5,
-    review: 'Review of Product B',
+    review: '⭐⭐',
     thumbnail: 'product/Hologram.png',
     threeDModel: 'product/hologram.glb'
   },
@@ -147,7 +147,7 @@ export const products = [
     Tagline: 'Blend new skills, sip your success!',
     description: "Elevate your career prospects with SkillShake, the innovative product that revolutionizes skill acquisition. Simply scan the job application requirements using the built-in camera, then blend your favorite ingredients into a delicious milkshake. Once you drink it, you'll instantly access the skills you need to stand out as if you have been practicing these skills for years!",
     stock: 8,
-    review: 'Review of Product C',
+    review: '⭐⭐⭐',
     thumbnail: 'product/Shake.png',
     threeDModel: 'product/skill-shake.glb'
   },
@@ -159,7 +159,7 @@ export const products = [
     Tagline: 'Study while you dream, wake up smarter.',
     description: 'The Dream Weaver is a groundbreaking sleep-study tool that turns your downtime into productive hours. Leveraging advanced neuro-audio technology, it optimizes your brain’s natural learning processes during sleep, making cramming for exams or absorbing new information effortless. Simply wear the Dream Weaver headband before bed, and wake up feeling more prepared and refreshed.',
     stock: 2,
-    review: 'Review of Product D',
+    review: '⭐⭐⭐⭐',
     thumbnail: 'product/Dream_Weaver.png',
     threeDModel: 'product/Dream_Weaver.glb'
   },
@@ -171,7 +171,7 @@ export const products = [
     Tagline: "Expand your brain’s storage",
     description: 'Introducing NeuroDrive, the cutting-edge device designed for students who aspire to transcend traditional learning methods. With its innovative brain-machine interface, NeuroDrive seamlessly connects your mind to a vast, high-speed storage system filled with encyclopedic knowledge.',
     stock: 4,
-    review: 'Review of Product E',
+    review: '⭐⭐',
     thumbnail: 'product/NeruoDrive.png',
     threeDModel: 'product/NeuroDrive.glb'
   },
